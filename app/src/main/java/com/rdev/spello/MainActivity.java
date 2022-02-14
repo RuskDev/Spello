02cbad7de7ba3edeb69f9178ead2c7474fa52a86
NathanUTwente <95768036+NathanUTwente@users.noreply.github.com>
2022-02-14 02:54:51 +0100

    Added a play button and made it open a second screen

--- app/src/main/java/com/rdev/spello/MainActivity.java
+++ app/src/main/java/com/rdev/spello/MainActivity.java
@@ -2,13 +2,30 @@ package com.rdev.spello;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.GnssAntennaInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button playButton = (Button) findViewById(R.id.playButton);
+        playButton.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent gameScreen = new Intent(this, GameActivity.class);
+        startActivity(gameScreen);
     }
 }
