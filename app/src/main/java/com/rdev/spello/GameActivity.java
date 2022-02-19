1b21c919e0c94e37809449c69fa06812405ad142
NathanUTwente <95768036+NathanUTwente@users.noreply.github.com>
2022-02-19 02:47:09 +0100

    Started functionality of first 10 keys
    Started GameMaster

--- app/src/main/java/com/rdev/spello/GameActivity.java
+++ app/src/main/java/com/rdev/spello/GameActivity.java
@@ -1,16 +1,75 @@
 package com.rdev.spello;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
 
     private GameMaster gameMaster;
+
+
+    private Button qKey = (Button) findViewById(R.id.qKey);
+    Button wKey = (Button) findViewById(R.id.wKey);
+    Button eKey = (Button) findViewById(R.id.eKey);
+    Button rKey = (Button) findViewById(R.id.rKey);
+    Button tKey = (Button) findViewById(R.id.tKey);
+    Button yKey = (Button) findViewById(R.id.yKey);
+    Button uKey = (Button) findViewById(R.id.uKey);
+    Button iKey = (Button) findViewById(R.id.iKey);
+    Button oKey = (Button) findViewById(R.id.oKey);
+    Button pKey = (Button) findViewById(R.id.pKey);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_activity);
+
+        setClickListeners();
+    }
+
+    private void setClickListeners(){
+        qKey.setOnClickListener(this);
+        wKey.setOnClickListener(this);
+        eKey.setOnClickListener(this);
+        rKey.setOnClickListener(this);
+        tKey.setOnClickListener(this);
+        yKey.setOnClickListener(this);
+        uKey.setOnClickListener(this);
+        iKey.setOnClickListener(this);
+        oKey.setOnClickListener(this);
+        pKey.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view){
+            case qKey:
+                gameMaster.keyPressed("Q");
+                break;
+            case wKey:
+                gameMaster.keyPressed("W");
+                break;
+            case eKey:
+                gameMaster.keyPressed("E");
+                break;
+            case rKey:
+                gameMaster.keyPressed("R");
+                break;
+            case tKey:
+                gameMaster.keyPressed("T");
+                break;
+            case yKey:
+                gameMaster.keyPressed("Y");
+                break;
+            case uKey:
+                gameMaster.keyPressed("U");
+                break;
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + view);
+        }
     }
 }
--- app/src/main/java/com/rdev/spello/GameMaster.java
+++ app/src/main/java/com/rdev/spello/GameMaster.java
@@ -1,4 +1,11 @@
 package com.rdev.spello;
 
+import android.view.View;
+
 public class GameMaster {
+
+    public void keyPressed(String let){
+
+    }
+
 }
