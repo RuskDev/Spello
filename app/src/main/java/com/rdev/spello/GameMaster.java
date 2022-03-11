9487122ab8bc968e3b61589165d721a623beeefb
NathanUTwente <95768036+NathanUTwente@users.noreply.github.com>
2022-03-11 21:31:54 +0100

    GameActivity functionality

--- app/src/main/java/com/rdev/spello/GameMaster.java
+++ app/src/main/java/com/rdev/spello/GameMaster.java
@@ -1,11 +1,29 @@
 package com.rdev.spello;
 
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class GameMaster {
+    TextView textView;
+    ArrayList<String> wordGuess;
+
+    public GameMaster(TextView textView) {
+        this.textView = textView;
+        wordGuess = new ArrayList<>();
+    }
 
     public void keyPressed(String let){
+        wordGuess.add(let);
+    }
 
+    public void updateView(){
+        String word = "";
+        for (String s : wordGuess){
+        word += s;
+        }
+        textView.setText(word);
     }
 
 }
