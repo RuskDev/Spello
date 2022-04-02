9db9559370c3c63a56bea758a7bfa71b36181926
NathanUTwente <95768036+NathanUTwente@users.noreply.github.com>
2022-04-02 23:53:17 +0200

    Added instant simple notification

--- app/src/main/java/com/rdev/spello/MainActivity.java
+++ app/src/main/java/com/rdev/spello/MainActivity.java
@@ -1,13 +1,21 @@
 package com.rdev.spello;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.content.res.Resources;
 import android.location.GnssAntennaInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Display;
@@ -23,19 +31,24 @@ import com.google.android.gms.ads.RequestConfiguration;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
+import java.security.PrivateKey;
 import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button button;
+    private Button button;
     private FrameLayout adContainerView;
     private AdView adView;
+
+    private final String CHANNEL_ID = "Spello";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createNotificationChannel();
 
         Button playButton = (Button) findViewById(R.id.playButton);
         playButton.setOnClickListener(this);
@@ -61,9 +74,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //start requesting banner ads
         loadBanner();
 
+        // Create an explicit intent for an Activity in your app
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.question_mark_icon)
+                .setContentTitle("My notification")
+                .setContentText("Hello World!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+// notificationId is a unique int for each notification that you must define
+        int notificationId = 1;
+        notificationManager.notify(notificationId, builder.build());
+
     }
 
-    //Todo actually make the homescreen
 
     @Override
     public void onClick(View view) {
@@ -99,5 +131,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Start loading the ad in the background.
         adView.loadAd(adRequest);
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            CharSequence name = getString(R.string.channel_name);
+            CharSequence name = "Spello";
+//            String description = getString(R.string.channel_description);
+            String description = "Spello Notification Channel";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
